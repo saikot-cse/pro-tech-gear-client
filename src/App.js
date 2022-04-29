c6fcc6c2e8088ccacb2c91aa714a07da99332816
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-04-29 08:00:06 +0600

    fake data added for design part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./Component/Shared/Navbar/NavBar";
+import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
       </Routes>
     </div>
   );
---
+++ src/Pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutIntro from '../../Component/AboutIntro/AboutIntro';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-center text-info my-5'>About Us</h1>
+      <AboutIntro/>
+    </div>
+  );
+};
+
+export default About;
